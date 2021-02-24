18074b4b103dee222887ed7e0421334b3f17e318
mahendrajastora <mohitjastora1539gmail.com>
2021-02-25 00:06:31 +0530

    comitting done

---
+++ q6.c
@@ -0,0 +1,25 @@
+/*
+Q6
+54321
+4321
+321
+21
+1
+
+*/
+
+#include<stdio.h>
+main()
+{
+    int i,j,n;
+    printf("n=");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+
+        for(j=i;j>=1;j--)
+            printf("%d",j);
+
+    printf("\n");
+    }
+}
