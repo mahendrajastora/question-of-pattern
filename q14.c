52b1493e1f9cba049fd8979224815eeaab44f436
mahendrajastora <mohitjastora1539gmail.com>
2021-02-25 00:05:03 +0530

    comitting done

---
+++ q14.c
@@ -0,0 +1,49 @@
+/*
+Q14
+
+   *
+   * *
+  * * *
+ * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+*/
+#include<stdio.h>
+int main()
+{
+	int i,j,k,n;
+	 printf("n=");
+    scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+	{
+		for(k=n-1;k>=i;k--)
+			{
+				printf(" ");
+			}
+		for(j=1;j<=i;j++)
+			{
+				printf("* ");
+			}
+
+		printf("\n");
+	}
+
+for(i=n-1;i>=1;i--)
+	{
+		for(k=n-1;k>=i;k--)
+			{
+				printf(" ");
+			}
+		for(j=i;j>=1;j--)
+			{
+				printf("* ");
+			}
+		printf("\n");
+	}
+
+return 0;
+}
