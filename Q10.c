8f73b7cf69a8081a47064d27945f7a8f0cc464a0
mahendrajastora <mohitjastora1539gmail.com>
2021-02-25 00:02:55 +0530

    comitting done

---
+++ Q10.c
@@ -0,0 +1,25 @@
+/*
+Q10
+    *
+   ***
+  *****
+ *******
+*********
+
+*/
+
+#include<stdio.h>
+int main(){
+int n;
+printf("n=");
+scanf("%d",&n);
+for(int i=0;i<=n;i++)
+{
+
+    for(int j=n-1;j>=i;j--)
+        printf(" ");
+    for(int k=1;k<=i;k++)
+            printf(" *");
+    printf("\n");
+}
+}
